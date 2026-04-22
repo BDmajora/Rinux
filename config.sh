e2d8b6412b6b9744d68a4b2fdb9ac3c0baec15d1
BDMajora <bdmajora@protonmail.com>
2026-04-21 19:18:02 -0600

    wine test 5

--- config.sh
+++ config.sh
@@ -14,12 +14,16 @@ cat <<EOF > "$CONFIG_DIR/init"
 $RINUX_BIN &
 
 # 2. Initialize Prefix & Set Registry for Wayland
-# Doing this inside init ensures the Wayland compositor is actually running!
 wine reg add "HKCU\Software\Wine\Drivers" /v Graphics /t REG_SZ /d "wayland,x11" /f
 
 # 3. Window Rules
-riverctl rule-add -app-id "wine*" float
+# REMOVED the 'float' rule so Wine natively tiles to fill the screen.
+# ADDED 'fullscreen' rule to force the Virtual Desktop to act as the root OS layer.
+riverctl rule-add -app-id "wine*" fullscreen
 riverctl csd-filter-add "wine*"
+
+# Remove River's native borders for a seamless look
+riverctl default-border-width 0
 
 # 4. Keybindings
 riverctl map normal Super Q close
@@ -27,7 +31,9 @@ riverctl map normal Super Return spawn foot
 riverctl map normal Super E exit
 
 # 5. Launch Wine Desktop
-env DISPLAY= wine explorer /desktop=Rinux,1280x800 explorer.exe &
+# REMOVED the trailing 'explorer.exe' so Wine loads the Shell (Taskbar/Start Menu) 
+# instead of a File Manager window.
+env DISPLAY= wine explorer /desktop=Rinux,1280x800 &
 
 riverctl background-color 0x4682b4
 EOF
