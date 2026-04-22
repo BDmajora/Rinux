e2e40ad851fc786ee1c9a16391035d71c57f8cfe
BDMajora <bdmajora@protonmail.com>
2026-04-22 00:12:49 -0600

    fixed terminal

--- config.sh
+++ config.sh
@@ -4,6 +4,8 @@
 CONFIG_DIR="$HOME/.config/river"
 mkdir -p "$CONFIG_DIR"
 RINUX_BIN="$HOME/Rinux/rinux-wm"
+# Setting the terminal to foot
+TERM_CMD="foot"
 
 cat <<EOF > "$CONFIG_DIR/init"
 #!/bin/sh
@@ -20,9 +22,12 @@ riverctl border-color-unfocused 0x586e75
 riverctl map normal Super Q close
 riverctl map normal Super E exit
 
+# Terminal Shortcut: Super + Enter (Return)
+riverctl map normal Super Return spawn $TERM_CMD
+
 # Default background
 riverctl background-color 0x002b36
 EOF
 
 chmod +x "$CONFIG_DIR/init"
-echo "River configuration reset. Wine components removed."
+echo "River configuration reset. Terminal set to foot (Super+Return)."
